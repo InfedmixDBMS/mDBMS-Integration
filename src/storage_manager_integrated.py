6dfc76804395258f35efd8605e6d4849ee2474b3
MaheswaraKaindra <19623197@std.stei.itb.ac.id>
2025-11-20 08:27:23 +0700

    feat: storage manager initial integration

--- src/storage_manager_integrated.py
+++ src/storage_manager_integrated.py
@@ -1,33 +1,52 @@
 from QueryProcessor.interfaces import AbstractStorageManager
+from StorageManager.classes import StorageEngine, Schema, DataRetrieval, DataWrite, DataDeletion
 from typing import List, Dict, Any, Optional
 from QueryProcessor.models import Rows
 
 class IntegratedStorageManager(AbstractStorageManager):
     def __init__(self):
-        pass
+        self.engine = StorageEngine()
     
-    def read_table(self, table_name: str, columns: Optional[List[str]] = None) -> Rows:
-        pass
+    # DataRetrieval: 
+    #   table -> str, 
+    #   column -> List[str], 
+    #   condition -> List[Condition]
+
+    # Condition: 
+    #   column -> str, 
+    #   operation -> enum('=', '<>', '>', '<', '>=', '<='), 
+    #   operand -> int | str | float
+
+    def read_block(self, data_retrieval: DataRetrieval) -> Rows:
+        return self.engine.read_block(data_retrieval)
     
-    def write_table(self, table_name: str, rows: Rows) -> int:
-        pass
+    # DataWrite: 
+    #   table -> str,
+    #   column -> List[str],
+    #   conditions -> List[Condition],
+    #   new_value -> List[T] | None
     
-    def update_rows(
-        self, 
-        table_name: str, 
-        updates: Dict[str, Any], 
-        condition: Optional[Dict[str, Any]] = None
-    ) -> int:
-        pass
+    def write_block(self, data_write: DataWrite) -> int:
+        return self.engine.write_block(data_write)
     
-    def delete_rows(self, table_name: str, condition: Optional[Dict[str, Any]] = None) -> int:
-        pass
+    # DataDeletion: 
+    #   table -> str,
+    #   conditions -> List[Condition]
+
+    def delete_block(self, data_deletion: DataDeletion) -> int:
+        return self.engine.delete_block(data_deletion)
     
-    def insert_rows(self, table_name: str, rows: Rows) -> int:
-        pass
+    # Schema:
+    #   columns:
+    #       "id" = IntType(),
+    #       "name" = VarCharType(50),
+    #       "ipk" = FloatType()
+    #   column_order: list(columns.keys())
+    #   column_index: {name: i for i, name in enumerate(self.column_order)}
+    #   size: int (auto-calculated, ga perlu dihitung manual)
     
-    def create_table(self, table_name: str, schema: Dict[str, str]) -> bool:
-        pass
+    def create_table(self, table_name: str, schema: Schema) -> bool:
+        return self.engine.create_table(table_name, schema)
     
     def drop_table(self, table_name: str) -> bool:
-        pass
+        return self.engine.drop_table(table_name)
